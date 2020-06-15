eb3b60098a099fa2cd8b4b72a9ddc23d775c9507
caterina <catemarian1806@gmail.com>
2020-06-15 09:39:35 +0200

    prova con css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 // chiedo all'utente il suo nome
-var nome=prompt("inserisci il proprio nome:");
+var nome=prompt("inserire il proprio nome:");
 // poi chiedi il suo cognome
-var cognome=prompt("inserisci il proprio cognome:");
+var cognome=prompt("inserire il proprio cognome:");
 // chiedi il suo colore preferito
-var colore=prompt("inserisci il proprio colore preferito:");
-// scrivi sulla pagina nomecognomecolorepreferito19
+var colore=prompt("inserire il proprio colore preferito:");
+// creare la password nomecognomecolorepreferito19
 var password=nome + cognome + colore + "19";
+// output in pagina :scrivi sulla pagina nomecognomecolorepreferito19
 document.getElementById('password').innerHTML="la password generata è: " + password;
+// uso console
+console.log("La password è stata generata con i dati inseriti")
